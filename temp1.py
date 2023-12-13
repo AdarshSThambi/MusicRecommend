53b9bb1629f80d6c087c5426ed653c731f2d390e
AdarshSThambi <adarshsthambi@gmail.com>
2023-12-13 21:45:42 +0530

    Deleted temp files

--- temp1.py
+++
@@ -1,9 +0,0 @@
-from bing_image_downloader import downloader
-from PIL import Image
-
-query_string = "shape of you sheeran"
-output_dir = 'dataset'
-# downloader.download(query_string, limit=1, output_dir=output_dir, adult_filter_off=True, force_replace=False,
-#                     timeout=20, verbose=True)
-im1 = Image.open('/'.join([output_dir,query_string,'Image_1.jpg']))
-im1.show()
